5672422f50ba0f93b4fa89b3d004ac71c37036ec
sabamgeliashvili <sba69717@gmail.com>
2023-11-11 15:51:25 +0400

    .

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-function buyItem(itemName, price) {
-    alert("Purchase Successful!\nProduct: " + itemName + "\nPrice: $" + price);
+let totalAmount = 0;
+
+function buyItem(itemName, itemPrice, itemImage) {
+    totalAmount += itemPrice;
+    
+    updateTotalAmount();
+
+    displayAddedItem(itemName, itemPrice, itemImage);
+}
+
+function updateTotalAmount() {
+    document.getElementById('sumcost').innerText = totalAmount + '$';
+}
+
+
+
+function displayAddedItem(itemName, itemPrice, itemImage) {
+    const addedItemDiv = document.createElement('div');
+    addedItemDiv.classList.add('added-item');
+    addedItemDiv.innerHTML = `
+        <p>${itemName}: ${itemPrice}$</p>
+        <img class="added-item-image" src="${itemImage}" alt="${itemName}">`;
+    document.getElementById('addeditem').appendChild(addedItemDiv);
+}
+
+
+function removeAddedItems() {
+    const addedItemContainer = document.getElementById('addeditem');
+    addedItemContainer.innerHTML = '';
+    totalAmount = 0;
+    updateTotalAmount();
 }
